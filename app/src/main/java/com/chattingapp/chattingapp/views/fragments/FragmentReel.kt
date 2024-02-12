2c39724e9ef2f2fcda71fef3146467ac48a4691f
Batu Uzun <103521291+BatuUzun@users.noreply.github.com>
2024-02-12 22:45:16 +0300

    Delete app directory

--- app/src/main/java/com/chattingapp/chattingapp/views/fragments/FragmentReel.kt
+++
@@ -1,9 +0,0 @@
-package com.chattingapp.chattingapp.views.fragments
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun FragmentReel() {
-    Text("Fragment Reel Content")
-}
